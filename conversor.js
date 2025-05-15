efa114c9550be93d42a343d18a72f20e456d90fc
Enrico Hidalgo <enricohidalgoperes11@gmail.com>
2025-05-15 18:52:16 -0300

    Criação do TempConversor
    TempConversor é um conversor de temperatura em HTML, CSS e JS, usando apenas lógica de programação.

---
+++ conversor.js
@@ -0,0 +1,29 @@
+document.getElementById('cvt').onclick = tempCvt;
+document.getElementById('cl').onclick = cltTxt;
+
+function tempCvt(){
+    let fahr = document.getElementById('Farenheit').value;
+    let cels = document.getElementById('Celsius').value;
+
+    if(isNaN(fahr) || isNaN(cels)){
+        alert("Digite um valor válido.");
+        return;
+    }else if(fahr === ''){
+        fahr = (parseFloat(cels) * 5/9) + 32;
+    }else if(cels === ''){
+        cels = (parseFloat(fahr) - 32) * 5/9;
+    }
+    if(fahr !== '' && cels !== '') {
+        alert("Digite apenas um dos campos.")
+    }
+    
+
+
+    document.getElementById('Farenheit').value = parseFloat(fahr).toFixed(2);
+    document.getElementById('Celsius').value = parseFloat(cels).toFixed(2);
+
+}
+function cltTxt(){
+    document.getElementById('Farenheit').value = "";
+    document.getElementById('Celsius').value = "";
+}
